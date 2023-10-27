76d75514af792727022819acbc9e5c245dd576e0
Nihal00 <nihallsuvarna@gmail.com>
2023-10-27 22:44:54 +0530

    frontend and backend update

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("users", UserSchema);
